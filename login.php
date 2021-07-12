ffc7f9f8049df98a52681d9cc4b765662b86cffa
Wilber Paredes <wilberparedes.intellinext@gmail.com>
2021-07-12 13:04:07 -0500

    change title

--- login.php
+++ login.php
@@ -16,8 +16,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SisSalud - Puerto Colombia</title>
-    <meta name="description" content="SisSalud - Puerto Colombia">
+    <title>Barranquilla Verde</title>
+    <meta name="description" content="Barranquilla Verde">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
